3b518dc12f4d53dfd60294d9cafe2ff20fc3ba44
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-12-12 17:17:10 -0300

    feat: adicionando AutoMapper

---
+++ Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/AutoMapper/AutoMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Sistema_Reserva_Restaurante.DTOs;
+using Sistema_Reserva_Restaurante.Models;
+using Sistema_Reserva_Restaurante.Service.Usuario;
+
+namespace Sistema_Reserva_Restaurante.AutoMapper
+{
+	public class AutoMapping : Profile
+	{
+		public AutoMapping()
+		{
+			Request();
+		}
+
+		private void Request()
+		{
+			CreateMap<UsuarioRegistroDto, Usuario>()
+				.ForMember(dest => dest.Senha, opt => opt.Ignore());
+		}
+	}
+}
