5395b2f01e9bc3c294972dcfc28c429c555aa823
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-12-12 17:15:12 -0300

    feat: adicionando regras de validação com Fluent Validation

---
+++ Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/Validations/UsuarioRegistroValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Sistema_Reserva_Restaurante.DTOs;
+using Sistema_Reserva_Restaurante.Exceptions;
+
+namespace Sistema_Reserva_Restaurante.Validations
+{
+	public class UsuarioRegistroValidator : AbstractValidator<UsuarioRegistroDto>
+	{
+		public UsuarioRegistroValidator()
+		{
+			RuleFor(u => u.Nome)
+				.NotEmpty().WithMessage(ResourceMessagesException.NOME_VAZIO);
+
+			RuleFor(u => u.Email)
+				.NotEmpty().WithMessage(ResourceMessagesException.EMAIL_VAZIO)
+				.EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALIDO);
+
+			RuleFor(u => u.Senha)
+				.NotEmpty().WithMessage(ResourceMessagesException.SENHA_VAZIA)
+				.MinimumLength(6).WithMessage(ResourceMessagesException.SENHA_INVALIDA);
+		}
+	}
+}
