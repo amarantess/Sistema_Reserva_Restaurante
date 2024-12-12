8bc49f0947d7080fed442ca91e25a579815a696b
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-12-12 17:06:51 -0300

    feat: adicionando dto para registrar usuário

---
+++ Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/DTOs/UsuarioRegistroDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Sistema_Reserva_Restaurante.DTOs
+{
+	public class UsuarioRegistroDto
+	{
+		public string Nome { get; set; }
+		public string Email { get; set; }
+		public string Senha { get; set; }
+		public string Role { get; set; } = "Cliente"; // Padrão: Cliente
+	}
+}
