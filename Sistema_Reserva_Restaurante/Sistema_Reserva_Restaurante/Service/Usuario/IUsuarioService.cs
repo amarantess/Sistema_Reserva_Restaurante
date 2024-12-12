15c2ae5174cb560d343f13e08fb76543d568bff9
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-12-12 17:10:12 -0300

    feat: adicionando interface do usuário

---
+++ Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/Service/Usuario/IUsuarioService.cs
@@ -0,0 +1,10 @@
+﻿using Sistema_Reserva_Restaurante.DTOs;
+using Sistema_Reserva_Restaurante.Models;
+
+namespace Sistema_Reserva_Restaurante.Service.Usuario
+{
+	public interface IUsuarioService
+	{
+		Task<ResponseRegistered>  CriarUsuario(UsuarioRegistroDto usuarioRegistroDto);
+	}
+}
