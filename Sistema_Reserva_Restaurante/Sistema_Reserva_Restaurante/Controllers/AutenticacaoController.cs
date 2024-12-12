83d9712650e08588835a2fe11c282feb63f07420
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-12-12 17:20:38 -0300

    feat: adicionando controller de autenticação

---
+++ Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/Controllers/AutenticacaoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_Reserva_Restaurante.DTOs;
+using Sistema_Reserva_Restaurante.Service.Usuario;
+
+namespace Sistema_Reserva_Restaurante.Controllers
+{
+	[Route("usuarios")]
+	[ApiController]
+	public class AutenticacaoController : ControllerBase
+	{
+		private readonly IUsuarioService _usuarioService;
+		public AutenticacaoController(IUsuarioService usuarioService)
+		{
+			_usuarioService = usuarioService;
+		}
+
+		[HttpPost("registrar")]
+		public async Task<ActionResult> Registrar(UsuarioRegistroDto usuarioRegistroDto)
+		{
+			var registro = await _usuarioService.CriarUsuario(usuarioRegistroDto);
+			return Created(string.Empty, registro);
+		}
+
+	}
+}
