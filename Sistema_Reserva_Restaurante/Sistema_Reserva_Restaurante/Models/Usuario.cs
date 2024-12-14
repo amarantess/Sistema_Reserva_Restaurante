ae0b8b3796c46734f312d56bac8d45a71a831fc9
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-12-13 22:58:04 -0300

    refactor: retirando a propriedade "role" do DTO de registrar usuário

--- Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/Models/Usuario.cs
+++ Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/Models/Usuario.cs
@@ -6,6 +6,6 @@
 		public string Nome { get; set; }
 		public string Email { get; set; }
 		public string Senha { get; set; }
-		public string Role { get; set; } // "Cliente" ou "Administrador"
+		public string Role { get; set; } = "Cliente"; // "Cliente" ou "Administrador"
 	}
 }
