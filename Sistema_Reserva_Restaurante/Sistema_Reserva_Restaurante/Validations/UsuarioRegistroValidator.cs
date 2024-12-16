1a5e3dfdaf34812ebe902c2267a2fef7dfe0ea91
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-12-16 18:44:09 -0300

    refactor: refatorando a classe UsuarioRegistroValidator

--- Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/Validations/UsuarioRegistroValidator.cs
+++ Sistema_Reserva_Restaurante/Sistema_Reserva_Restaurante/Validations/UsuarioRegistroValidator.cs
@@ -12,12 +12,17 @@ namespace Sistema_Reserva_Restaurante.Validations
 				.NotEmpty().WithMessage(ResourceMessagesException.NOME_VAZIO);
 
 			RuleFor(u => u.Email)
-				.NotEmpty().WithMessage(ResourceMessagesException.EMAIL_VAZIO)
-				.EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALIDO);
+				.NotEmpty().WithMessage(ResourceMessagesException.EMAIL_VAZIO);
 
 			RuleFor(u => u.Senha)
 				.NotEmpty().WithMessage(ResourceMessagesException.SENHA_VAZIA)
 				.MinimumLength(6).WithMessage(ResourceMessagesException.SENHA_INVALIDA);
+
+			When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+			{
+				RuleFor(u => u.Email)
+				.EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALIDO);
+			});
 		}
 	}
 }
